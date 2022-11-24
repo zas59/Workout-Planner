467bf2fa3d0f00ad340ce3c105b47f491952c8b7
Jake Bram <jhb94@txstate.edu>
2022-11-24 15:36:20 -0600

    Fixed some linting errors, testing first github commit. Happy Turkey Day

--- workout.py
+++ workout.py
@@ -1,8 +1,8 @@
+'''Workout Planning App by Zach and Jake. Name TBD, TXST SWE, EIR Laith Hasanian.'''
 import random
 import os
 from urllib.parse import urlparse, urljoin
 import requests
-import psycopg2
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
@@ -14,12 +14,14 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def find_workouts():
+    '''Function for Landing Page'''
     return render_template('search_workout.html')
 
 @app.route('/handle_workout_search', methods=['GET', 'POST'])
 def handle_workout_search():
+    '''Search for and render workout suggestions.'''
     form_data = request.form
-    API_URL = 'https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises'
+    api_url = 'https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises'
     api_key = os.getenv('X_API_KEY')
     headers = {
 	"X-RapidAPI-Key": api_key,
@@ -27,7 +29,7 @@ def handle_workout_search():
     }
     querystring = {"muscle": form_data['targetArea'], "difficulty": form_data['difficulty']}
     response = requests.get(
-        API_URL,
+        api_url,
         headers=headers,
         params=querystring
     )
@@ -37,8 +39,9 @@ def handle_workout_search():
 
 @app.route('/handle_workout_selection', methods=['GET', 'POST'])
 def handle_workout_selection():
+    '''Handle a users selection of workouts'''
     form_data = request.form
-    API_URL = 'https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises'
+    api_url = 'https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises'
     api_key = os.getenv('X_API_KEY')
     headers = {
 	"X-RapidAPI-Key": api_key,
@@ -46,15 +49,15 @@ def handle_workout_selection():
     }
     querystring = {'name': form_data['chosenWorkout']}
     response = requests.get(
-        API_URL,
+        api_url,
         headers=headers,
         params=querystring
     )
     json_data = response.json()
     workout_obj = json_data
-    chosenWorkout = ""
+    chosen_workout = ""
     for workout in workout_obj:
         if str(form_data['chosenWorkout']) == str(workout['name']):
-            chosenWorkout = workout['name']
-            return render_template('show_chosen_workout.html', workout = chosenWorkout)
+            chosen_workout = workout['name']
+            return render_template('show_chosen_workout.html', workout = chosen_workout)
 app.run()
